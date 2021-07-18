77c47108961ba8556d06ab27b6aae114e931a0f2
Chuck Danielsson <chuck.danielsson@gmail.com>
2021-07-18 17:25:44 -0400

    install material-ui

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders deboint in the document', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/deboint/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-function App() {
+// TODO - is this 100vh applied across the board the best approach?
+const useStyles = makeStyles((_theme) => ({
+  outerDiv: {
+    height: '100vh',
+  },
+  gridContainer: {
+    height: '100vh',
+  },
+  headline: {
+    textAlign: 'center',
+  },
+}));
+
+/**
+ * Colorscheme
+ *
+ * Red - #d24c31
+ * Gold - #cac06b
+ * oWhite - #edecd8
+ * Blue - #7ecfd9
+ * Slate - #628486
+ */
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <React.Fragment>
+      <CssBaseline />
+      <div className={classes.outerDiv}>
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          className={classes.gridContainer}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Grid item xs={12}>
+            <Typography variant="h1" className={classes.headline}>
+              deboint
+            </Typography>
+          </Grid>
+        </Grid>
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default App;
