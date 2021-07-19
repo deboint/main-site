1a3946538c13a3fe5b54038cd60c555700e0baa3
Chuck Danielsson <chuck.danielsson@gmail.com>
2021-07-18 21:12:26 -0400

    Add links to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
 
 // TODO - is this 100vh applied across the board the best approach?
-const useStyles = makeStyles((_theme) => ({
+const useStyles = makeStyles((theme) => ({
   outerDiv: {
     height: '100vh',
   },
@@ -15,6 +17,14 @@ const useStyles = makeStyles((_theme) => ({
   headline: {
     textAlign: 'center',
   },
+  blurb: {
+    textAlign: 'center',
+    padding: theme.spacing(2),
+  },
+  link: {
+    textDecoration: 'none',
+    color: theme.palette.text.primary
+  }
 }));
 
 /**
@@ -44,8 +54,18 @@ const App = () => {
             <Typography variant="h1" className={classes.headline}>
               deboint
             </Typography>
-            <Typography variant="subtitle1" className={classes.headline}>
-              <a href="mailto:chuck@deboint.com">contact</a>
+            <Typography variant="subtitle1" className={classes.blurb}>
+              Solutions Engineered
+            </Typography>
+            <Typography variant="subtitle2" className={classes.headline}>
+              <a href="https://github.com/deboint" className={classes.link}>
+                <GitHubIcon />
+              </a>
+            </Typography>
+            <Typography variant="subtitle2" className={classes.headline}>
+              <a href="mailto:chuck@deboint.com" className={classes.link}>
+                <MailOutlineIcon />
+              </a>
             </Typography>
           </Grid>
         </Grid>
